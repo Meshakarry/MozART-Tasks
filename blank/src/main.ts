10e5da4d8b8476337c8d81d7e7fa917045aff93d
dinabjelic <75682425+dinabjelic@users.noreply.github.com>
2021-05-18 01:22:24 +0200

    Main.ts

--- blank/src/main.ts
+++ blank/src/main.ts
@@ -1,13 +1,14 @@
-import Vue from 'nativescript-vue'
-import App from './components/App.vue'
-import Home from './components/Home.vue'
-import { Label } from 'tns-core-modules/ui/label';
-import { isAndroid } from 'tns-core-modules/platform';
-import DictatsCompositions from './components/DictatsCompositions.vue'; 
+ 
+import Vue from'nativescript-vue'
+import App from'./components/App.vue'
+import Home from'./components/Home.vue'
+import {​​​​​ Label }​​​​​ from'tns-core-modules/ui/label';
+import {​​​​​ isAndroid }​​​​​ from'tns-core-modules/platform';
+import DictatsCompositions from'./components/DictatsCompositions.vue'; 
 import Diktati from './components/Diktati.vue';
-import thirdCategory from './components/thirdCategory.vue';
+import thirdCategory from'./components/thirdCategory.vue';
 import Puzzle from './components/Puzzle.vue';
-import PogodiInstrument from './components/PogodiInstrument.vue';
+import PogodiInstrument from'./components/PogodiInstrument.vue';
 import Memori from './components/Memori.vue';
 import GameBox from './components/GameBox.vue'
 import PogodiInstrument4 from './components/PogodiInstrument4.vue';
@@ -18,22 +19,8 @@ import AnalSlusanjeTrozvuka4 from './components/AnalSlusanjeTrozvuka4.vue';
 
 
 
+Vue.registerElement("VideoPlayer", () =>require("nativescript-videoplayer").Video);
+new Vue({​​​​​
+render:h=>h('frame', [h(PogodiInstrument)])
+}​​​​​).$start()
 
-
-Vue.registerElement("VideoPlayer", () => require("nativescript-videoplayer").Video);
-
-new Vue({
-
-  
-  render: h => h('frame', [h(AnalSlusanjeTrozvuka)])
-}).$start()
-
-export function onLabelLoaded(args) {
-  const lbl=args.object as Label;
-  
-    lbl.android.setGravity(17);
-  
-}
-
-  render: h => h('frame', [h(PogodiInstrument)])
-}).$start()
